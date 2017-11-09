2c24aa1b1c97448f57e43b8ba53c36dcf6ce0406
Ash Ramesh <ashramesh1992@gmail.com>
2017-11-09 21:33:30 +1100

    Add Company model and migration files

--- paranuara/companies/models.py
+++ paranuara/companies/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Company(models.Model):
+    """A company on the planet of Paranuara"""
+
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=255)
