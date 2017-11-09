7227ede077bf0fe36d0f979ca9e6fa95e827bca5
Ash Ramesh <ashramesh1992@gmail.com>
2017-11-09 22:25:11 +1100

    Add people app to settings

--- paranuara/settings.py
+++ paranuara/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'paranuara.companies'
+    'paranuara.companies',
+    'paranuara.people',
 ]
 
 MIDDLEWARE = [
