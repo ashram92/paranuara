b4fb3706bdda424a1c7c04420acf6e40135a777f
Ash Ramesh <ashramesh1992@gmail.com>
2017-11-09 22:10:33 +1100

    Add __str__ method for readability

--- paranuara/companies/models.py
+++ paranuara/companies/models.py
@@ -6,3 +6,6 @@ class Company(models.Model):
 
     id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return '<{}: {}>'.format(self.name, self.id)
