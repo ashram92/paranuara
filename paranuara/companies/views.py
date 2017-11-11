432e1e2f4655ed167971277639a316cf935ab879
Ash Ramesh <ashramesh1992@gmail.com>
2017-11-11 20:07:14 +1100

    Create view to query company employees

--- paranuara/companies/views.py
+++ paranuara/companies/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import serializers
+from rest_framework.generics import RetrieveAPIView
 
-# Create your views here.
+from paranuara.companies.models import Company
+from paranuara.people.serializers import PersonSerializer
+
+
+class CompanySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Company
+        fields = ('id', 'name', 'employees')
+
+    employees = PersonSerializer(many=True)
+
+
+class CompanyEmployeesAPIView(RetrieveAPIView):
+
+    queryset = Company.objects
+    serializer_class = CompanySerializer
