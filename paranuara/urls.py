4610f6fad3de1079e96519d515e57be470848b35
Ash Ramesh <ashramesh1992@gmail.com>
2017-11-11 23:35:32 +1100

    Remove admin urls

--- paranuara/urls.py
+++ paranuara/urls.py
@@ -17,7 +17,6 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
     url(r'companies/', include('paranuara.companies.urls',
                                namespace='company-urls')),
     url(r'people/', include('paranuara.people.urls',
